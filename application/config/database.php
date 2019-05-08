2156afb4f55729ca4a283206fda19bb3655044c2
Rakotomamonjy Nasandratra <rakotomamonjynasandratra@gmail.com>
2019-05-08 07:34:44 +0100

    nan

--- application/config/database.php
+++ application/config/database.php
@@ -73,33 +73,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
-	'database' => 'zoofilm',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
 // $db['default'] = array(
 // 	'dsn'	=> '',
-// 	'hostname' => 'remotemysql.com',
-// 	'username' => 'kb7OKPsVgW',
-// 	'password' => '48M0s3eGy9',
-// 	'database' => 'kb7OKPsVgW',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => 'root',
+// 	'database' => 'zoofilm',
 // 	'dbdriver' => 'mysqli',
 // 	'dbprefix' => '',
 // 	'pconnect' => FALSE,
@@ -115,3 +94,24 @@ $db['default'] = array(
 // 	'failover' => array(),
 // 	'save_queries' => TRUE
 // );
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'remotemysql.com',
+	'username' => 'kb7OKPsVgW',
+	'password' => '48M0s3eGy9',
+	'database' => 'kb7OKPsVgW',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
